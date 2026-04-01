c474bfe161d2d8a2decdedeb10add4771247a591
Evan <evan.allender@gmail.com>
2026-03-31 22:23:29 -0400

    ch2: add basic routes and middleware

--- main.go
+++ main.go
@@ -1,6 +1,32 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+	"sync/atomic"
+)
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(writer http.ResponseWriter, request *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(writer, request)
+	})
+}
+
+func (cfg *apiConfig) serverMetrics(writer http.ResponseWriter, request *http.Request) {
+	writer.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	writer.WriteHeader(200)
+	writer.Write([]byte(fmt.Sprintf("Hits: %d", cfg.fileserverHits.Load())))
+}
+
+func (cfg *apiConfig) resetMetrics(writer http.ResponseWriter, request *http.Request) {
+	writer.WriteHeader(200)
+	cfg.fileserverHits.Store(0)
+}
 
 func healthz(writer http.ResponseWriter, request *http.Request) {
 	writer.Header().Set("Content-Type", "text/plain; charset=utf-8")
@@ -10,8 +36,11 @@ func healthz(writer http.ResponseWriter, request *http.Request) {
 
 func main() {
 	serveMux := http.NewServeMux()
-	serveMux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
-	serveMux.HandleFunc("/healthz", healthz)
+	apiCfg := apiConfig{fileserverHits: atomic.Int32{}}
+	serveMux.Handle("/app/", http.StripPrefix("/app", apiCfg.middlewareMetricsInc(http.FileServer(http.Dir(".")))))
+	serveMux.HandleFunc("GET /healthz", healthz)
+	serveMux.HandleFunc("GET /metrics", apiCfg.serverMetrics)
+	serveMux.HandleFunc("POST /reset", apiCfg.resetMetrics)
 
 	server := http.Server{}
 	server.Addr = ":8080"
