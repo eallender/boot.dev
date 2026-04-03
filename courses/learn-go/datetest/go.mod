e17179a7065ab9ff161df6bcbc51f8c91b52cace
Evan <evan.allender@gmail.com>
2026-04-02 21:00:32 -0400

    courses: add learn-go course files

---
+++ courses/learn-go/datetest/go.mod
@@ -0,0 +1,5 @@
+module example.com/username/datetest
+
+go 1.24.5
+
+require github.com/wagslane/go-tinytime v0.0.2 // indirect
---
+++ courses/learn-go/hellogo/main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+	"example.com/username/mystrings"
+)
+
+func main() {
+	fmt.Println(mystrings.Reverse("hello world"))
+}
